e05f3bf75646dfb5c4b29a6205ef88c9abdeb2e0
alan <2092459@qq.com>
2018-11-14 11:44:18 +0800

    add sample folder
    add sample folder

---
+++ sample/components/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="app">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	{
+		name:'app',
+		data:function(){return{
+			msg:'hello world!'
+		}}
+	}
+</script>
+
+<style>
+	.app{
+		background: lightblue;
+	}
+</style>
---
+++ sample/index.js
@@ -0,0 +1,6 @@
+Vue.CptsLoader.isSaveTemplateToLocalStorage = false
+Vue.CptsLoader.ProjectVersion = "1.0.0.0"
+var vm = Vue.create({
+	el:"#app",
+	components:['components/app']
+})
